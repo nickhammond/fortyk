7b1496982ca7f27b85e30f3c976696bd39697b24
Nick Hammond <nrhammond@gmail.com>
2010-03-03 21:12:51 -0700

    finish basic number parsing to shorten numbers

---
+++ lib/fourtyk.rb
@@ -0,0 +1,53 @@
+module Fourtyk
+  def shorten_number
+    
+    if self.is_a?(String)
+      self.gsub!(/[a-zA-z$,]/,"")
+      num = self.to_i.round
+      string = num.to_s
+    else
+      num = self.to_i
+      string = num.to_s
+    end
+    
+    case string.length
+    when 4..6 
+      label = "K"
+      upper = 4
+      lower = -3
+    when 7..9 
+      label = "M"
+      upper = 7
+      lower = -6
+    when 10..12 
+      label = "B"
+      upper = 10
+      lower = -9
+    else
+      label = nil
+    end
+    
+    if label
+      first = string[0..string.length - upper].to_i
+      last = string[lower..lower+2].to_i * 0.001
+      last = sprintf("%.1f", last).to_f
+      num = first + last
+      num = num.round if last.zero? || last == 1
+    end
+    
+    "#{num}#{label}"
+  end
+  
+  def shorten_money(options = {})
+    options[:currency] ||= "$"
+    "#{options[:currency]}#{self.shorten_number}"
+  end
+end
+
+class Numeric
+  include Fourtyk
+end
+
+class String
+  include Fourtyk
+end
--- test/test_fourtyk.rb
+++ test/test_fourtyk.rb
@@ -1,7 +1,33 @@
 require 'helper'
 
 class TestFourtyk < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "default shorten number examples" do
+    EXAMPLES.each do |example|
+      should "convert #{example[0]} to #{example[1]}" do
+        assert_equal example[0].shorten_number, example[1]
+      end
+    end    
+  end
+  context "currency" do
+    should "put the $ sign in front" do
+      assert_equal 4000.shorten_money, "$4K"
+      assert_equal 4000.shorten_money(:currency => "$"), "$4K"
+    end
+    
+    should "put the # sign in front" do
+      assert_equal 4000.shorten_money(:currency => "#"), "#4K"
+    end
+    
+    should "display 1 point" do 
+      assert_equal 4100.shorten_money, "$4.1K"
+    end
+  end
+  context "precision" do
+    should "display 1 point" do
+      assert_equal 4100.shorten_number, "4.1K"
+    end
+    should "display 0 points" do 
+      assert_equal 4000.shorten_number, "4K"
+    end
   end
 end
